01fd7908ad465ad13f5511c57b87ab69a4b39be0
anubhavbablic <anubhav@bablic.com>
2018-03-27 17:19:30 +0530

    Setting up Runmode

--- src/test/java/com/bablic/testcases/LoginUser.java
+++ src/test/java/com/bablic/testcases/LoginUser.java
@@ -2,20 +2,28 @@ package com.bablic.testcases;
 
 import java.util.Hashtable;
 
+import org.testng.SkipException;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
 
+import com.bablic.base.Page;
 import com.bablic.pages.Login_Form;
 import com.bablic.pages.Dashboard.AddNewSite;
 import com.bablic.utility.CommonUtilities;
 
 @Guice
-public class LoginUser {
+public class LoginUser extends Page{
 	
 	
 	@Test(dataProviderClass= CommonUtilities.class,dataProvider="dp")
 	
 	public void loginuser(Hashtable<String, String> data) throws InterruptedException{
+		
+		if(!CommonUtilities.isTestRunnable("enter_credentials", excel)){
+			throw new SkipException("Run Mode set to NO");
+			}
+				
+			
 		
 		Login_Form lf=new Login_Form();
 		AddNewSite add=lf.enter_credentials(data.get("Email"),data.get("Password"));
