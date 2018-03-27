38bb6aa0139083a7a1c5ff3b845da49633fcaa69
anubhavbablic <anubhav@bablic.com>
2018-03-27 17:30:41 +0530

    Setting up Runmode for testcases V

--- src/main/java/com/bablic/pages/Login_Form.java
+++ src/main/java/com/bablic/pages/Login_Form.java
@@ -15,11 +15,7 @@ public class Login_Form extends Page{
 	
 	public AddNewSite enter_credentials(String Email, String Password) throws InterruptedException {
 		
-	if(!CommonUtilities.isTestRunnable("enter_credentials", excel)){
-		throw new SkipException("Run Mode set to NO");
-		}
-			
-		
+	
 
 		Thread.sleep(20L);
 
